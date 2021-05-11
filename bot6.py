d3dc207812d56121bb9e44ae76307f5936e3e603
Sidh22 <81886864+Sidh22@users.noreply.github.com>
2021-05-10 22:51:51 -0400

    Add files via upload

---
+++ bot6.py
@@ -0,0 +1,231 @@
+from __future__ import unicode_literals
+import discord
+from discord.ext import commands
+from pybooru import Danbooru
+from operator import itemgetter
+from kekw.danb import *
+import PARAMETERS
+
+no_dupe = []
+banned_tags = 'male_focus'
+server = discord.Client()
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+client = commands.Bot(command_prefix='?', intents=intents)
+cli = Danbooru('danbooru', username=PARAMETERS.DANBOORU_USER, api_key=PARAMETERS.DANBOORU_TOKEN)
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+
+@client.command(aliases=['h'])
+async def user_help(ctx):
+    e = discord.Embed(title='Helper', color=0x0e0de6)
+    helper(e)
+    await ctx.send(embed=e)
+
+
+@client.command(aliases=['tag', 't'])
+async def tagged(ctx, *, tags='hololive'):
+    try:
+        posts = cli.post_list(tags=f'{tags}', limit=1)
+        for posted in posts:
+            e = discord.Embed(title='Tagged', color=0x0e0de6)
+            image_embed(e, posted)
+            no_dupe.append(posted['id'])
+
+            await ctx.send(embed=e)
+    except IndexError:
+        w = discord.Embed(title='InaDisappoint', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+    except KeyError:
+        w = discord.Embed(title='KeyError: I know how to fix but it\'s aids', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+    except ValueError:
+        w = discord.Embed(title='ValueError', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+
+
+@client.command(aliases=['rt', 'rts'])
+async def ran_tag_saf(ctx, *, tags='-boys_only', rating='s'):
+    posts = cli.post_list(tags=f'{tags}', limit=40, random=True)
+    for n in range(0, 40):
+        try:
+            if banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
+                    and posts[n]['id'] not in no_dupe[-10:]:
+                e = discord.Embed(title='Tagged Safe', color=0x2ae20c)
+                image_embed_multiple(e, posts, n)
+                no_dupe.append(posts[n]['id'])
+
+                await ctx.send(embed=e)
+                break
+        except KeyError:
+            pass
+        except IndexError:
+            w = discord.Embed(title='IndexError: Input a usable tag, retard', color=0x2ae20c)
+            ina_embed(w)
+            await ctx.send(embed=w)
+            break
+
+
+@client.command(aliases=['rtq'])
+async def ran_tag_que(ctx, *, tags='-boys_only', rating='q'):
+    posts = cli.post_list(tags=f'{tags}', limit=100, random=True)
+    for n in range(0, 100):
+        try:
+            if banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
+                    and posts[n]['id'] not in no_dupe[-10:]:
+                e = discord.Embed(title='Tagged Questionable', color=0xee8f10)
+                image_embed_multiple(e, posts, n)
+                no_dupe.append(posts[n]['id'])
+
+                await ctx.send(embed=e)
+                break
+        except KeyError:
+            pass
+        except IndexError:
+            w = discord.Embed(title='IndexError: Input a usable tag, retard', color=0x2ae20c)
+            ina_embed(w)
+            await ctx.send(embed=w)
+            break
+
+
+@client.command(aliases=['rte'])
+async def ran_tag_ex(ctx, *, tags='-boys_only', rating='e'):
+    posts = cli.post_list(tags=f'{tags}', limit=40, random=True)
+    for n in range(0, 40):
+        try:
+            if banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
+                    and posts[n]['id'] not in no_dupe[-10:]:
+                e = discord.Embed(title='Tagged Explicit', color=0xe20c0c)
+                image_embed_multiple(e, posts, n)
+                no_dupe.append(posts[n]['id'])
+
+                await ctx.send(embed=e)
+                break
+        except KeyError:
+            pass
+        except IndexError:
+            w = discord.Embed(title='IndexError: Input a usable tag, retard', color=0x2ae20c)
+            ina_embed(w)
+            await ctx.send(embed=w)
+            break
+
+
+@client.command(aliases=['r', 'random'])
+async def pure_random(ctx):
+    posts = cli.post_list(limit=1, tags='-boys_only', random=True)
+    e = discord.Embed(title='Random', color=0x0b0b0c)
+    image_embed_multiple(e, posts, 0)
+
+    await ctx.send(embed=e)
+
+
+@client.command(aliases=['pop', 'popular'])
+async def popular_post(ctx, *, page='1'):
+    try:
+        page = int(page)
+        post = cli.post_list(limit=1, tags='order:rank', page={page})
+        for posted in post:
+            e = discord.Embed(title='Popular Post', color=0xFF00FF)
+            image_embed(e, posted)
+
+            await ctx.send(embed=e)
+    except KeyError:
+        w = discord.Embed(title='KeyError: lole go somewhere else', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+    except ValueError:
+        w = discord.Embed(title='ValueError: Enter a number, retard', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+
+
+@client.command(aliases=['tbig', 'tpop'])
+async def tagged_popular(ctx, *, tags='-boys_only'):
+    try:
+        if tags.isnumeric():
+            w = discord.Embed(title='Number\'s don\'t work, idiot.', color=0x2ae20c)
+            ina_embed(w)
+            await ctx.send(embed=w)
+        posts = cli.post_list(tags=f'{tags}', limit=100)
+        updoot = []
+        for n in range(0, 100):
+            updoot.append(posts[n]['up_score'])
+        tied = tuple(zip(posts, updoot))
+        s_u = sorted(tied, key=itemgetter(1))
+        s_u.reverse()
+        for n in range(0, 100):
+            try:
+                if 'mp4' not in s_u[n][0]['file_url'][-3:] and 'ebm' not in s_u[n][0]['file_url'][-3:] \
+                        and s_u[n][0]['id'] not in no_dupe[-10:]:
+                    post = s_u[n][0]
+                    e = discord.Embed(title='Most Popular Tagged', color=0x2ae20c)
+                    image_embed(e, post)
+                    no_dupe.append(s_u[n][0]['id'])
+
+                    await ctx.send(embed=e)
+                    break
+            except KeyError:
+                pass
+            except IndexError:
+                break
+    except IndexError:
+        w = discord.Embed(title='IndexError', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+
+
+@client.command(aliases=['tebig', 'tepop'])
+async def tagged_popular_explicit(ctx, *, tags='-boys_only', rating='e'):
+    try:
+        posts = cli.post_list(tags=f'{tags}', limit=100)
+        updoot = []
+        for n in range(0, 100):
+            updoot.append(posts[n]['up_score'])
+        tied = tuple(zip(posts, updoot))
+        s_u = sorted(tied, key=itemgetter(1))
+        s_u.reverse()
+        for n in range(0, 100):
+            try:
+                if 'mp4' not in s_u[n][0]['file_url'][-3:] and 'ebm' not in s_u[n][0]['file_url'][-3:] \
+                        and s_u[n][0]['rating'] in rating and banned_tags not in s_u[n][0]['tag_string_general'] \
+                        and s_u[n][0]['id'] not in no_dupe[-10:]:
+                    post = s_u[n][0]
+                    e = discord.Embed(title='Most Popular Tagged', color=0x2ae20c)
+                    image_embed(e, post)
+                    no_dupe.append(s_u[n][0]['id'])
+
+                    await ctx.send(embed=e)
+                    break
+            except KeyError:
+                print(f'KeyError: ?tepop {tags}')
+                pass
+            except IndexError:
+                print(f'IndexError: ?tepop {tags}')
+                pass
+    except IndexError:
+        w = discord.Embed(title='IndexError', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+    except ValueError:
+        w = discord.Embed(title='ValueError', color=0x2ae20c)
+        ina_embed(w)
+        await ctx.send(embed=w)
+
+
+@client.event
+async def on_message(message):  # need regex
+    await client.process_commands(message)
+    if 'feet' in message.content or 'Feet' in message.content:
+        if not message.content.startswith('?'):
+            await message.channel.send(feet())
+    if 'cum' in message.content or 'Cum' in message.content or 'COOM' in message.content or 'coom' in message.content:
+        await message.channel.send(cum())
+
+
+client.run(PARAMETERS.BOT_TOKEN)
---
+++ danb.py
@@ -0,0 +1,104 @@
+import logging
+
+bot_url = 'https://i.redd.it/74tcw6iq1k161.jpg'
+ina_url = 'https://preview.redd.it/307b6pt6tfx61.jpg?width=232&format=pjpg&auto=webp&s' \
+          '=ef88bc0611dc25cbf388b5717b3ab4801401a614 '
+megumin_url = 'https://static.wikia.nocookie.net/rehero/images/4/44/Megumin.jpg/revision/latest/scale-to-width-down' \
+              '/310?cb=20180330001646 '
+aqua_url = 'https://danbooru.donmai.us/data/sample/bb/9d' \
+           '//__aqua_kono_subarashii_sekai_ni_shukufuku_wo_drawn_by_kuro_mushi__sample' \
+           '-bb9d727bf996c86554dce5c2cb9198e7.jpg '
+darkness_url = "https://danbooru.donmai.us/data/original/98/29" \
+               "/__darkness_kono_subarashii_sekai_ni_shukufuku_wo_drawn_by_lambda_kusowarota__9829baa99fcbf3b61ac2552ecdb34824.jpg "
+
+
+def logg():
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%d-%m-%Y:%H:%M:%S',
+                        level=logging.DEBUG,
+                        filename='logs.txt')
+
+
+def helper(embed):
+    embed.set_image(url=bot_url)
+    embed.add_field(name='?t (tag)', value='Used to find post of specific tag', inline=True)
+    embed.add_field(name='?rts (tag)', value='Used to find a random, tagged, safe post', inline=True)
+    embed.add_field(name='?rtq (tag)', value='Used to find a random, tagged, questionable post', inline=True)
+    embed.add_field(name='?rte (tag)', value='Used to find a random, tagged, explicit post', inline=True)
+    embed.add_field(name='?r', value='Used to find a random post, independent of tag(Always not dudes)', inline=True)
+    embed.add_field(name='?pop #', value='Used to find popular/hot posts', inline=True)
+    embed.add_field(name='?tpop (tag)', value='Used to find most upvoted post of given tag', inline=True)
+    embed.add_field(name='?h', value='Help', inline=True)
+    logg()
+
+
+def cum():
+    borpa = "<:Borpa:838413361603018792>"
+    f = (f"{borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM \n"
+         f"{borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM \n"
+         f"{borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM \n"
+         f"{borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM {borpa} CUM ")
+    return f
+
+
+def feet():
+    f = ("***SNIFFFFFFFFFFFFFFFFFFFFFFFFFFF***\n"
+         "***...breathing heavily...***\n"
+         "***I LOVE FEET***")
+    return f
+
+
+def ina_embed(embed):
+    embed.set_image(url=ina_url)
+    embed.add_field(name='You idiot', value='Stop screwing it up')
+    logg()
+
+
+def image_embed(embed, post):
+    if 'jpg' in post['file_url'][-3:] or 'png' in post['file_url'][-3:] or 'gif' in post['file_url'][-3:]:
+        embed.set_image(url=post['file_url'])
+    else:
+        embed.set_image(url=ina_url)
+    embed.add_field(name='Post ID', value=post['id'], inline=True)
+    if post['rating'] == 's':
+        rating = 'Safe'
+        embed.set_thumbnail(url=megumin_url)
+    elif post['rating'] == 'q':
+        rating = 'Questionable'
+        embed.set_thumbnail(url=aqua_url)
+    elif post['rating'] == 'e':
+        rating = 'Explicit'
+        embed.set_thumbnail(url=darkness_url)
+    if post['tag_string_character']:
+        embed.add_field(name='Character', value=post['tag_string_character'].split(' ')[0], inline=True)
+    else:
+        embed.add_field(name='Character', value='Original', inline=True)
+    embed.add_field(name='Rating', value=rating, inline=True)
+    embed.add_field(name='Upvotes', value=post['up_score'], inline=True)
+    embed.add_field(name='Downvotes', value=post['down_score'], inline=True)
+    logg()
+
+
+def image_embed_multiple(embed, post, n):
+    if 'jpg' in post[n]['file_url'][-3:] or 'png' in post[n]['file_url'][-3:] or 'gif' in post[n]['file_url'][-3:]:
+        embed.set_image(url=post[n]['file_url'])
+    else:
+        embed.set_image(url=ina_url)
+    embed.add_field(name='Post ID', value=post[n]['id'], inline=True)
+    if post[n]['rating'] == 's':
+        rating = 'Safe'
+        embed.set_thumbnail(url=megumin_url)
+    elif post[n]['rating'] == 'q':
+        rating = 'Questionable'
+        embed.set_thumbnail(url=aqua_url)
+    elif post[n]['rating'] == 'e':
+        rating = 'Explicit'
+        embed.set_thumbnail(url=darkness_url)
+    if post[n]['tag_string_character']:
+        embed.add_field(name='Character', value=post[n]['tag_string_character'].split(' ')[0], inline=True)
+    else:
+        embed.add_field(name='Character', value='Original', inline=True)
+    embed.add_field(name='Rating', value=rating, inline=True)
+    embed.add_field(name='Upvotes', value=post[n]['up_score'], inline=True)
+    embed.add_field(name='Downvotes', value=post[n]['down_score'], inline=True)
+    logg()
