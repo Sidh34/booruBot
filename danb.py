daec4b25febffb7de35a5823f45b32a886528ae3
Sidh34 <81886864+Sidh34@users.noreply.github.com>
2021-05-11 12:58:47 -0400

    Update danb.py

--- danb.py
+++ danb.py
@@ -1,12 +1,6 @@
 import logging
 import PARAMETERS
 
-bot_url = PARAMETERS.BOT_URL
-ina_url = PARAMETERS.INA_URL
-megumin_url = PARAMETERS.MEGUMIN_URL
-aqua_url = PARAMETERS.AQUA_URL
-darkness_url = PARAMETERS.DARKNESS_URL
-
 banned_tags = 'male_focus'
 feet_pattern = '[fF].{2}[eE]*.{0,1}[tT]'
 accepted_file_types = '(png|jpg|gif)'
@@ -21,7 +15,7 @@ def logg():
 
 
 def helper(embed):  # needs finishing
-    embed.set_image(url=bot_url)
+    embed.set_image(url=PARAMETERS.BOT_URL)
     embed.add_field(name='?t (tag)', value='Used to find post of specific tag', inline=True)
     embed.add_field(name='?rts (tag)', value='Used to find a random, tagged, safe post', inline=True)
     embed.add_field(name='?rtq (tag)', value='Used to find a random, tagged, questionable post', inline=True)
@@ -33,16 +27,8 @@ def helper(embed):  # needs finishing
     logg()
 
 
-def c_t():
-    return PARAMETERS.T_COPY_PASTA
-
-
-def f_t():
-    return PARAMETERS.F_COPY_PASTA
-
-
 def ina_embed(embed):
-    embed.set_image(url=ina_url)
+    embed.set_image(url=PARAMETERS.INA_URL)
     embed.add_field(name='You idiot', value='Stop screwing it up')
     logg()
 
@@ -51,17 +37,17 @@ def image_embed_multiple(embed, post, n):
     if 'jpg' in post[n]['file_url'][-3:] or 'png' in post[n]['file_url'][-3:] or 'gif' in post[n]['file_url'][-3:]:
         embed.set_image(url=post[n]['file_url'])
     else:
-        embed.set_image(url=ina_url)
+        embed.set_image(url=PARAMETERS.INA_URL)
     embed.add_field(name='Post ID', value=post[n]['id'], inline=True)
     if post[n]['rating'] == 's':
         rating = 'Safe'
-        embed.set_thumbnail(url=megumin_url)
+        embed.set_thumbnail(url=PARAMETERS.MEGUMIN_URL)
     elif post[n]['rating'] == 'q':
         rating = 'Questionable'
-        embed.set_thumbnail(url=aqua_url)
+        embed.set_thumbnail(url=PARAMETERS.AQUA_URL)
     elif post[n]['rating'] == 'e':
         rating = 'Explicit'
-        embed.set_thumbnail(url=darkness_url)
+        embed.set_thumbnail(url=PARAMETERS.DARKNESS_URL)
     else:
         rating = 'Unknown'
     if post[n]['tag_string_character']:
