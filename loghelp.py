791c2ca57260e25755753053cf6289214fb2b7be
Sidh34 <81886864+Sidh34@users.noreply.github.com>
2021-05-23 21:19:34 -0400

    Update loghelp.py

--- loghelp.py
+++ loghelp.py
@@ -1,4 +1,6 @@
 import logging
+import ast
+import random
 
 
 def logg():
@@ -11,9 +13,23 @@ def logg():
 def saver(post, n):
     try:
         data = dict(Character=post[n]["tag_string_character"].split(" ")[0], Rating=post[n]["rating"],
-                    ImageUrl=post[n]["file_url"])
+                    ImageUrl=post[n]["file_url"], Upvotes=post[n]['up_score'])
         with open('saving.txt', 'a+') as f:
             f.write(f'{data}\n')
     except ValueError:
         pass
+
+
+def opener(character='keqing_(genshin_impact)'):
+    w = len(open('saving.txt').readlines())
+    rat = []
+    with open('saving.txt', 'r+') as f:
+        lines = [line[:-1] for line in f]
+        for p in range(0, w):
+            res = ast.literal_eval(lines[p])
+            if res['Character'] in character:
+                rat.append(res)
+    elem = random.randint(0, len(rat)-1)
+    return rat[elem]
+
 
