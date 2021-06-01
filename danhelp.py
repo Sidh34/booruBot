c5a11b658feb7fdfd648fdf6fadb5e3f19f87af7
Sidh34 <81886864+Sidh34@users.noreply.github.com>
2021-06-01 19:33:52 -0400

    Create danhelp.py

---
+++ danhelp.py
@@ -0,0 +1,39 @@
+from pybooru import Danbooru
+import PARAMETERS
+from functools import lru_cache
+
+cli = Danbooru('danbooru', username=PARAMETERS.DANBOORU_USER, api_key=PARAMETERS.DANBOORU_TOKEN)
+
+
+@lru_cache(maxsize=20)
+def small_post_getter(tags):
+    posts = cli.post_list(tags=f'{tags}', limit=2)
+    return posts
+
+
+@lru_cache(maxsize=1000)
+def large_post_getter(tags):
+    posts = cli.post_list(tags=f'{tags}', limit=100)
+    return posts
+
+
+@lru_cache(maxsize=1000)
+def random_post_getter(tags):
+    posts = cli.post_list(tags=f'{tags}', limit=100, random=True)
+    return posts
+
+
+@lru_cache(maxsize=100)
+def id_post_getter(identity):
+    posts = cli.post_show(identity)
+    return posts
+
+
+def small_random_post_getter():
+    posts = cli.post_list(limit=1, tags='-boys_only', random=True)
+    return posts
+
+
+def popular_post_getter(page):
+    posts = cli.post_list(limit=2, tags='order:rank', page={page})
+    return posts
