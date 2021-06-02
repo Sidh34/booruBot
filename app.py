274c6d44599b0b394dd23890a703d1969550bcc2
Sidh34 <81886864+Sidh34@users.noreply.github.com>
2021-06-02 00:13:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,24 +24,28 @@ async def user_help(ctx):
 
 @client.command(aliases=['tag', 't'])  # saved to no dupe
 async def tagged(ctx, *, tags='hololive'):
+    start_time = time.time()
     try:
         posts = small_post_getter(tags)
         for n in range(0, 2):
             if posts[n]['id'] not in no_dupe[-15:] and \
                     re.search(accepted_file_types, posts[n]['file_ext']):
                 e = tagged_disc_embed(posts, n)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
             elif posts[n]['id'] not in no_dupe[-15:] and \
                     re.search(partially_accepted, posts[n]['file_ext']):
                 e = tagged_disc_embed(posts, n)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
             elif posts[n]['id'] not in no_dupe[-15:] and \
                     re.search(zip_replace, posts[n]['large_file_url'][-4:]):
                 e = tagged_disc_embed(posts, n)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
@@ -61,6 +65,7 @@ async def tagged(ctx, *, tags='hololive'):
 
 @client.command(aliases=['rt', 'rts'])  # saved to no dupe
 async def ran_tag_saf(ctx, *, tags='-boys_only', rating='s'):
+    start_time = time.time()
     posts = random_post_getter(tags)
     for n in range(0, 100):
         try:
@@ -68,6 +73,7 @@ async def ran_tag_saf(ctx, *, tags='-boys_only', rating='s'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
@@ -75,6 +81,7 @@ async def ran_tag_saf(ctx, *, tags='-boys_only', rating='s'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{posts[n]['file_url']}")
@@ -83,6 +90,7 @@ async def ran_tag_saf(ctx, *, tags='-boys_only', rating='s'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{posts[n]['large_file_url']}")
@@ -98,6 +106,7 @@ async def ran_tag_saf(ctx, *, tags='-boys_only', rating='s'):
 
 @client.command(aliases=['rtq'])  # saved to no dupe
 async def ran_tag_que(ctx, *, tags='-boys_only', rating='q'):
+    start_time = time.time()
     posts = random_post_getter(tags)
     for n in range(0, 100):
         try:
@@ -105,6 +114,7 @@ async def ran_tag_que(ctx, *, tags='-boys_only', rating='q'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
@@ -112,6 +122,7 @@ async def ran_tag_que(ctx, *, tags='-boys_only', rating='q'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{posts[n]['file_url']}")
@@ -120,6 +131,7 @@ async def ran_tag_que(ctx, *, tags='-boys_only', rating='q'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{posts[n]['large_file_url']}")
@@ -135,6 +147,7 @@ async def ran_tag_que(ctx, *, tags='-boys_only', rating='q'):
 
 @client.command(aliases=['rte'])  # saved to no dupe
 async def ran_tag_exp(ctx, *, tags='-boys_only', rating='e'):
+    start_time = time.time()
     posts = random_post_getter(tags)
     for n in range(0, 100):
         try:
@@ -142,6 +155,7 @@ async def ran_tag_exp(ctx, *, tags='-boys_only', rating='e'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
@@ -149,6 +163,7 @@ async def ran_tag_exp(ctx, *, tags='-boys_only', rating='e'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{posts[n]['file_url']}")
@@ -157,6 +172,7 @@ async def ran_tag_exp(ctx, *, tags='-boys_only', rating='e'):
                     banned_tags not in posts[n]['tag_string_general'] and posts[n]['rating'] in rating \
                     and posts[n]['id'] not in no_dupe[-15:]:
                 e = ran_tagged_discord_embed(posts, n, rating)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{posts[n]['large_file_url']}")
@@ -172,11 +188,13 @@ async def ran_tag_exp(ctx, *, tags='-boys_only', rating='e'):
 
 @client.command(aliases=['r', 'random'])  # not saved to no_dupe
 async def pure_random(ctx):
+    start_time = time.time()
     posts = small_random_post_getter()
     try:
         if re.search(accepted_file_types, posts[0]['file_ext']):
             e = discord.Embed(title='Random', color=0x0b0b0c)  # saver
             image_embed_multiple(e, posts, 0)
+            e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
             await ctx.send(embed=e)
     except KeyError:
@@ -191,23 +209,27 @@ async def pure_random(ctx):
 
 @client.command(aliases=['pop', 'popular'])  # not saved to no_dupe
 async def popular_post(ctx, *, page='1'):
+    start_time = time.time()
     try:
         page = int(page)
         post = popular_post_getter(page)
         for n in range(0, 2):
             if re.search(accepted_file_types, post[n]['file_ext']):
                 e = order_rank_discord_embed(post, n)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 break
             elif re.search(partially_accepted, post[n]['file_ext']):
                 e = order_rank_discord_embed(post, n)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{post[n]['file_url']}")
                 break
             elif re.search(zip_replace, post[n]['large_file_url'][-4:]):
                 e = order_rank_discord_embed(post, n)
+                e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                 await ctx.send(embed=e)
                 await ctx.send(f"{post[n]['large_file_url']}")
@@ -224,6 +246,7 @@ async def popular_post(ctx, *, page='1'):
 
 @client.command(aliases=['tbig', 'tpop'])  # saved to no_dupe
 async def tagged_popular(ctx, *, tags='-boys_only'):
+    start_time = time.time()
     try:
         posts = large_post_getter(tags)
         updoot = []
@@ -237,12 +260,14 @@ async def tagged_popular(ctx, *, tags='-boys_only'):
                 if re.search(accepted_file_types, s_u[n][0]['file_ext']) \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_popular_discord_embed(s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     break
                 elif re.search(partially_accepted, s_u[n][0]['file_ext']) \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_popular_discord_embed(s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['file_url']}")
@@ -250,6 +275,7 @@ async def tagged_popular(ctx, *, tags='-boys_only'):
                 elif re.search(zip_replace, s_u[n][0]['large_file_url'][-4:]) \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_popular_discord_embed(s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['large_file_url']}")
@@ -270,6 +296,7 @@ async def tagged_popular(ctx, *, tags='-boys_only'):
 
 @client.command(aliases=['tebig', 'tepop'])  # saved to no_dupe
 async def tagged_popular_explicit(ctx, *, tags='-boys_only', rating='e'):
+    start_time = time.time()
     try:
         posts = large_post_getter(tags)
         updoot = []
@@ -287,6 +314,7 @@ async def tagged_popular_explicit(ctx, *, tags='-boys_only', rating='e'):
                         and s_u[n][0]['rating'] in rating and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     break
@@ -294,6 +322,7 @@ async def tagged_popular_explicit(ctx, *, tags='-boys_only', rating='e'):
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['file_url']}")
@@ -302,6 +331,7 @@ async def tagged_popular_explicit(ctx, *, tags='-boys_only', rating='e'):
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['large_file_url']}")
@@ -322,6 +352,7 @@ async def tagged_popular_explicit(ctx, *, tags='-boys_only', rating='e'):
 
 @client.command(aliases=['tqbig', 'tqpop'])  # saved to no_dupe
 async def tagged_popular_questionable(ctx, *, tags='-boys_only', rating='q'):
+    start_time = time.time()
     try:
         posts = large_post_getter(tags)
         updoot = []
@@ -339,6 +370,7 @@ async def tagged_popular_questionable(ctx, *, tags='-boys_only', rating='q'):
                         s_u[n][0]['rating'] in rating and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     break
@@ -346,6 +378,7 @@ async def tagged_popular_questionable(ctx, *, tags='-boys_only', rating='q'):
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['file_url']}")
@@ -354,6 +387,7 @@ async def tagged_popular_questionable(ctx, *, tags='-boys_only', rating='q'):
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['large_file_url']}")
@@ -392,13 +426,15 @@ async def tagged_popular_safe(ctx, *, tags='-boys_only', rating='s'):
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
-                    await ctx.send(embed=e), print("--- %s seconds ---" % (time.time() - start_time))
+                    await ctx.send(embed=e)
                     break
                 elif re.search(partially_accepted, s_u[n][0]['file_ext']) and s_u[n][0]['rating'] in rating \
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['file_url']}")
@@ -407,6 +443,7 @@ async def tagged_popular_safe(ctx, *, tags='-boys_only', rating='s'):
                         and banned_tags not in s_u[n][0]['tag_string_general'] \
                         and s_u[n][0]['id'] not in no_dupe[-15:]:
                     e = tagged_pop_rate_discord_embed(rating, s_u, n)
+                    e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
                     await ctx.send(embed=e)
                     await ctx.send(f"{s_u[n][0]['large_file_url']}")
@@ -440,22 +477,26 @@ async def tagged_popular_safe(ctx, *, tags='-boys_only', rating='s'):
 
 @client.command(aliases=['id'])
 async def id_find_info(ctx, *, identity):
+    start_time = time.time()
     try:
         post = id_post_getter(identity)
         if re.search(accepted_file_types, post['file_ext']) and banned_tags not in post['tag_string_general']:
             e = discord.Embed(title='ID Grab', color=0x2ae20c)  # not logg or saver
             individual_embed(e, post)
+            e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
             await ctx.send(embed=e)
         elif re.search(partially_accepted, post['file_ext']) and banned_tags not in post['tag_string_general']:
             e = discord.Embed(title='ID Grab', color=0x2ae20c)  # not logg or saver
             individual_embed(e, post)
+            e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
             await ctx.send(embed=e)
             await ctx.send(f"{post['file_url']}")
         elif re.search(zip_replace, post['large_file_url'][-4:]) and banned_tags not in post['tag_string_general']:
             e = discord.Embed(title='ID Grab', color=0x2ae20c)  # not logg or saver
             individual_embed(e, post)
+            e.set_footer(text="--- %s seconds ---" % (time.time() - start_time))
 
             await ctx.send(embed=e)
             await ctx.send(f"{post['large_file_url']}")
