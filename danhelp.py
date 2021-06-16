f66065ee341b947f223345b5a1b670728ca06469
Sidney Hunt <81886864+Sidh34@users.noreply.github.com>
2021-06-16 14:19:58 -0400

    Update danhelp.py

--- danhelp.py
+++ danhelp.py
@@ -5,25 +5,25 @@ from functools import lru_cache
 cli = Danbooru('danbooru', username=PARAMETERS.DANBOORU_USER, api_key=PARAMETERS.DANBOORU_TOKEN)
 
 
-@lru_cache(maxsize=20)
+@lru_cache(maxsize=3)
 def small_post_getter(tags):
     posts = cli.post_list(tags=f'{tags}', limit=2)
     return posts
 
 
-@lru_cache(maxsize=250)
+@lru_cache(maxsize=3)
 def large_post_getter(tags):
     posts = cli.post_list(tags=f'{tags}', limit=100)
     return posts
 
 
-@lru_cache(maxsize=250)
+@lru_cache(maxsize=3)
 def random_post_getter(tags):
     posts = cli.post_list(tags=f'{tags}', limit=100, random=True)
     return posts
 
 
-@lru_cache(maxsize=100)
+@lru_cache(maxsize=3)
 def id_post_getter(identity):
     posts = cli.post_show(identity)
     return posts
